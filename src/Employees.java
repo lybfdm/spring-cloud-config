a0b4086f0bbe3940433b3ec2598ae26488858292
Clint <lybfdm@126.com>
2022-03-13 17:53:27 -0400

    idea git practise

---
+++ src/Employees.java
@@ -0,0 +1,13 @@
+public class Employees {
+    int employee_id;
+    String employee_name;
+    int employee_salary;
+
+    Employees(){}
+
+    Employees(int id, String name, int salary){
+        this.employee_id=id;
+        this.employee_name=name;
+        this.employee_salary=salary;
+    }
+}
